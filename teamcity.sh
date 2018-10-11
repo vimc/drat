2e6e54e8baf64410d70ed6d29f43d00e756979f7
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-10-11 14:40:35 +0100

    More fields for the commit script

--- teamcity.sh
+++ teamcity.sh
@@ -2,11 +2,14 @@
 set -ex
 
 GIT_BRANCH=$(git symbolic-ref --short HEAD)
-if [ "$GIT_BRANCH" = "master" ]; then
+if [ "$GIT_BRANCH" = "i1594" ]; then
     docker pull richfitz/drat.builder
     docker run --rm \
+           --entrypoint bash -it \
            -e GIT_AUTHOR_EMAIL="$(git config --get user.email)" \
            -e GIT_AUTHOR_NAME="$(git config --get user.name)" \
+           -e GIT_COMMITTER_EMAIL="$(git config --get user.email)" \
+           -e GIT_COMMITTER_NAME="$(git config --get user.name)" \
            -v ${PWD}:/src \
            richfitz/drat.builder
     # NOTE: teamcity will have a https remote so override this with a
